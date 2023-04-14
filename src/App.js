2f9029a068ca2ac857453a40820eefc3de2c37af
AnPham <phamhaian2001@gmail.com>
2023-04-15 00:33:40 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import Foods from "./Components/Foods";
+import classNames from "classnames/bind";
+import styles from "./Styles.module.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+var cx = classNames.bind(styles);
 
 function App() {
   return (
     <div className="App">
-      
+      <header className={cx('container')}>
+        <button className={cx('menu')}>
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+        <div className={cx('header')}>
+          <h2>My Food</h2>
+        </div>
+        <div className={cx('mail')}>
+          <h2>Mail</h2>
+        </div>
+      </header>
+      <div>
+        <Foods />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Foods.js
@@ -0,0 +1,32 @@
+function Foods() {
+    return ( 
+        <div>
+            <div>
+                <img src="https://cdn.beptruong.edu.vn/wp-content/uploads/2013/01/pho-mai-que.jpg" alt="Phô mai que"/>
+            </div>
+            <div>
+                <img src="https://cdn.tgdd.vn/2021/12/CookDishThumb/cach-lam-bun-dau-mam-tom-ngon-ngat-ngay-an-mot-lan-la-ghien-thumb-620x620.jpg" alt="Bún đậu"/>
+            </div>
+            <div>
+                <img src="https://thammyviennevada.com/wp-content/uploads/2020/10/an-bun-cha-co-beo-khong-1.jpg" alt="bún chả"/>
+            </div>
+            <div>
+                <img src="https://cdn.tgdd.vn/Files/2018/12/06/1136063/lam-mon-com-rang-kim-chi-chuan-vi-han-trong-15-phut-202110221426203480.jpg" alt="Cơm rang"/>
+            </div>
+            <div>
+                <img src="https://cattour.vn/images/upload/images/quan-buffet-lau-99k-o-ha-noi/lau-phan.jpg" alt="Cơm trộn"/>
+            </div>
+            <div>
+                <img src="https://img.dominos.vn/Pizzaminsea-Hai-San-Nhiet-Doi-Xot-Tieu.jpg" alt="Pizza"/>
+            </div>
+            <div>
+                <img src="https://cdn.tgdd.vn/2022/01/CookRecipe/Avatar/xoi-xeo-gao-nep-nau-bang-noi-com-dien-thumbnail.jpg" alt="Xôi xéo"/>
+            </div>
+            <div>
+                <img src="https://cleverjunior.vn/wp-content/uploads/2022/08/gioi-thieu-banh-mi-bang-tieng-anh-1.jpg" alt="Bánh mỳ"/>
+            </div>
+        </div>
+    );
+}
+
+export default Foods;
